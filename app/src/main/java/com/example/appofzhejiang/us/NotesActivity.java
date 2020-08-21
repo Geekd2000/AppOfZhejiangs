15f0cfe696bf138dd66454ef671fce252e9efbca
Genetizens <3362836098@qq.com>
2020-08-21 16:48:46 +0800

    添加了我的游记这一页面

---
+++ app/src/main/java/com/example/appofzhejiang/us/NotesActivity.java
@@ -0,0 +1,206 @@
+package com.example.appofzhejiang.us;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.appofzhejiang.MainActivity;
+import com.example.appofzhejiang.R;
+
+import java.util.List;
+
+public class NotesActivity extends AppCompatActivity {
+    private static final int CHOOSE_PHOTO = 2;
+
+    private Dialog mShareDialog;
+    private ImageButton button;
+    private ImageView imageView,imageView2;
+    private List<ImageView> imageViews;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+
+        button = findViewById(R.id.imageButton);
+        imageView = findViewById(R.id.image);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(NotesActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.
+                        PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(NotesActivity.this, new
+                            String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1);
+                } else {
+
+                    openAlbum();
+
+                }
+            }
+        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                showDialog();// 单击按钮后 调用显示视图的 showDialog 方法
+                return true;
+            }
+        });
+    }
+
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.
+                        PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    handleImageOnKitKat(data);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            //如果是document类型的Uri，则通过documentID处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID+"="+id;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+                }
+            }else if("com.android.providers.downloads.document".equals(uri.
+                    getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloas"),Long.valueOf(docId));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    imagePath = getImagePath(contentUri,null);
+                }
+            }
+        }else if("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+    private void displayImage(String imagePath) {
+        if(imagePath != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            imageView.setImageBitmap(bitmap);
+            if(imageView.getVisibility() == View.INVISIBLE){
+                imageView.setVisibility(View.VISIBLE);
+            }
+        }else{
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private String getImagePath(Uri externalContentUri, String selection) {
+        String path =null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Cursor cursor = getContentResolver().query(externalContentUri, null, selection, null, null);
+            if(cursor != null){
+                if(cursor.moveToFirst()){
+                    path = cursor.getString(cursor.getColumnIndex(MediaStore.
+                            Images.Media.DATA));
+                }
+                cursor.close();
+            }
+        }
+        return path;
+    }
+
+    private void showDialog() {
+        if (mShareDialog == null) {
+            initShareDialog();
+        }
+        mShareDialog.show();
+    }
+    /**
+     * 初始化分享弹出框
+     */
+    private void initShareDialog() {
+        mShareDialog = new Dialog(this, R.style.dialog_bottom_full);
+        mShareDialog.setCanceledOnTouchOutside(true); //手指触碰到外界取消
+        mShareDialog.setCancelable(true);             //可取消 为true
+        Window window = mShareDialog.getWindow();      // 得到dialog的窗体
+        window.setGravity(Gravity.BOTTOM);
+        window.setWindowAnimations(R.style.share_animation);
+
+        View view = View.inflate(this, R.layout.lay_share_1, null); //获取布局视图
+        view.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mShareDialog != null && mShareDialog.isShowing()) {
+                    mShareDialog.dismiss();
+                }
+                imageView.setVisibility(View.INVISIBLE);
+            }
+        });
+        window.setContentView(view);
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);//设置横向全屏
+    }
+}
