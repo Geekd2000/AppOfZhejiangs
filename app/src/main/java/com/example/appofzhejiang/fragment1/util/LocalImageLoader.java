6cc52bd5857ec01dde402db20d0354852d4a15cd
QYGK <HZNU_QYGK@163.com>
2020-09-02 20:00:31 +0800

    首页改动

--- app/src/main/java/com/example/appofzhejiang/fragment1/util/LocalImageLoader.java
+++ app/src/main/java/com/example/appofzhejiang/fragment1/util/LocalImageLoader.java
@@ -7,20 +7,19 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
 import com.youth.banner.loader.ImageLoader;
 
-import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
 
 
 public class LocalImageLoader extends ImageLoader {
 
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         Glide.with(context)
                 .load(path)
+                .dontAnimate()
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
                 .into(imageView);
     }
 }
